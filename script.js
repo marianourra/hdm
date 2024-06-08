42fd9abf35ac5b0da27e632ce7c09915a6a41269
marianourra <marianourradev@gmail.com>
2024-06-08 04:50:30 -0300

    sacar animacion texto

--- script.js
+++ script.js
@@ -17,22 +17,4 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    const textElement = document.getElementById('typed-text');
-    const text = textElement.textContent;
-    textElement.textContent = '';
-    
-    let index = 0;
-    function type() {
-        if (index < text.length) {
-            textElement.textContent += text.charAt(index);
-            index++;
-            setTimeout(type, 50); // Adjust typing speed here (milliseconds)
-        } else {
-            textElement.style.borderRight = "none"; // Remove the cursor after typing is done
-        }
-    }
-
-    type();
-});
 
