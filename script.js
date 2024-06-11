2a2a618c70a473d3ee5ed1b1adc7b6ada2077f53
marianourra <marianourradev@gmail.com>
2024-06-11 10:28:43 -0300

    correccion cards

--- script.js
+++ script.js
@@ -19,16 +19,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function toggleDescription(card) {
     card.classList.toggle('show-description');
-    const img = card.querySelector('img');
     const cardText = card.querySelector('.card-text');
     if (card.classList.contains('show-description')) {
-        img.style.filter = 'grayscale(0%)';
-        cardText.style.maxHeight = cardText.scrollHeight + "px"; // Dynamically set the max-height to the scrollHeight of the element
+        cardText.style.maxHeight = cardText.scrollHeight + "px"; // Set max-height to scrollHeight
     } else {
-        img.style.filter = 'grayscale(100%)';
         cardText.style.maxHeight = "0";
     }
 }
+
 
 
 
